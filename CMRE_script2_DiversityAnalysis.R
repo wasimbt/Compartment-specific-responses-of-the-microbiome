ad2e9c561b30618bdcb212e07fb94614fb6ee039
wasimbt <wasim.bt@gmail.com>
2023-12-15 15:44:02 +0100

    Update CMRE_script2_DiversityAnalysis.R

--- CMRE_script2_DiversityAnalysis.R
+++ CMRE_script2_DiversityAnalysis.R
@@ -22,16 +22,11 @@
 # library(devtools); packageVersion("devtools")
 # ## [1] ‘2.4.3’
 # library(pairwiseAdonis)
-##
 
 ####################################--------rarefaction at 8100----------####################
-
 set.seed(301)
-
 CMRE5_r8100 = rarefy_even_depth(CMRE5, sample.size = 8100, rngseed = TRUE) #rarefaction
-
 CMRE5_r8100_t  = transform_sample_counts(CMRE5_r8100, function(x) x / sum(x) ) # transforming sample count (converting into relative abundance)
-
 
 ##################################-------Alpha diversity  --------------###################
 
@@ -40,22 +35,13 @@ CMRE5_r8100_t  = transform_sample_counts(CMRE5_r8100, function(x) x / sum(x) ) #
 CMRE5_r8100_richness <- estimate_richness(CMRE5_r8100, measures = c("Observed", "Shannon"))
 
 ###########-Here metadata table is merged with alpha diversity table in order to easy downstream analysis. 
-
 CMRE5_r8100_richness$sample_names <- rownames(CMRE5_r8100_richness)
-
 CMRE5_r8100_Metadata_table = data.frame(as(sample_data(CMRE5_r8100), "data.frame"))
-
 CMRE5_r8100_Metadata_table$sample_names <- rownames(CMRE5_r8100_Metadata_table)
-
 CMRE5_Metadata_with_AlphaDiv_r8100 <- CMRE5_r8100_Metadata_table %>% left_join(CMRE5_r8100_richness)
-
-head(CMRE5_Metadata_with_AlphaDiv_r8100)
-
 rownames(CMRE5_Metadata_with_AlphaDiv_r8100) <- CMRE5_Metadata_with_AlphaDiv_r8100$sample_names
 
-
 ##################################------Running Alpha diversity Linear models------------------#################
-
 Observed <- (CMRE5_alpha_r8100$Observed)
 Treatment <- (CMRE5_alpha_r8100$Treatment)
 Timepoint <- (CMRE5_alpha_r8100$Timepoint)
@@ -67,47 +53,33 @@ Fisher <- (CMRE5_alpha_r8100$Fisher)
 
 ##################---------------Observed
 lm_M1Observed_8100_I <- aov(Observed ~  Treatment * as.factor(Timepoint) * as.factor(Concentration) * Sampletype + Lib , na.action=na.fail)
-
 Anova(lm_M1Observed_8100_I, type="II")
 
 ##############---------- Tukey on linear model
-
 lm_M1Observed_8100Tukey <- TukeyHSD(lm_M1Observed_8100_I) 
 
 ##################---------------Shannon
-
 lm_M1Shannon_8100_I <- aov(Shannon ~  Treatment * as.factor(Timepoint) * as.factor(Concentration) * Sampletype + Lib , na.action=na.fail)
-
 Anova(lm_M1Shannon_8100_I, type="II")
 
 ##############---------- Tukey on linear model
-
 lm_M1Shannon_8100Tukey <- TukeyHSD(lm_M1Shannon_8100_I) 
-
 
 ######################################################################################################
 ###########################----------------Beta diversity analysis------------########################
-
 ########################--Bray-curtis 
-
 set.seed(301)
 CMRE5_r8100_t_br <- distance(CMRE5_r8100_t, method = "bray") 
-
 CMRE5_r8100_t_br_adonis1 <- adonis(CMRE5_r8100_t_br ~  Treatment * as.factor(Timepoint) * as.factor(Concentration) * Sampletype + Lib, as(sample_data(CMRE5_r8100_t), "data.frame"), permutations = 999) #
 
 ########################--Jaccard 
-
 set.seed(301)
 CMRE5_r8100_t_Ja <- distance(CMRE5_r8100_t, method = "jaccard") 
-
 CMRE5_r8100_t_Ja_adonis1 <- adonis(CMRE5_r8100_t_Ja ~  Treatment * as.factor(Timepoint) * as.factor(Concentration) * Sampletype + Lib, as(sample_data(CMRE5_r8100_t), "data.frame"), permutations = 999) #
 
 ##########-- Pairwise adonis tests (Bray)----#########
-
 CMRE5_r8100_t_metadata <- sample_data(CMRE5_r8100_t)
-
 CMRE5_r8100_t_br_adonis2 <- pairwise.adonis(CMRE5_r8100_t_br,  CMRE5_r8100_t_metadata$Treatment,  CMRE5_r8100_t_metadata$Sampletype) #
-# 
 
 Treatment <-  CMRE5_r8100_t_metadata$Treatment
 Timepoint <-  CMRE5_r8100_t_metadata$Timepoint
@@ -115,7 +87,6 @@ Concentration <-  CMRE5_r8100_t_metadata$Concentration
 Sampletype <-  CMRE5_r8100_t_metadata$Sampletype
 
 # Now prepare the interaction terms as variable to test
-
 TTCS <- interaction(Treatment,  as.factor(Timepoint) , as.factor(Concentration) , Sampletype )
 TTC <- interaction(Treatment,  as.factor(Timepoint) , as.factor(Concentration) )
 TTS <- interaction(Treatment,  as.factor(Timepoint) , Sampletype )
@@ -141,10 +112,7 @@ CMRE5_r8100_t_br_adonis_TS1 <- pairwise.adonis(CMRE5_r8100_t_br,  TS1)
 CMRE5_r8100_t_br_adonis_CS <- pairwise.adonis(CMRE5_r8100_t_br,  CS)
 
 ##########-- Pairwise adonis tests (Jaccard)----#########
-
-
 CMRE5_r8100_t_metadata <- sample_data(CMRE5_r8100_t)
-
 CMRE5_r8100_t_ja_adonis2 <- pairwise.adonis(CMRE5_r8100_t_Ja,  CMRE5_r8100_t_metadata$Treatment,  CMRE5_r8100_t_metadata$Sampletype) #
 
 Treatment <-  CMRE5_r8100_t_metadata$Treatment
@@ -153,7 +121,6 @@ Concentration <-  CMRE5_r8100_t_metadata$Concentration
 Sampletype <-  CMRE5_r8100_t_metadata$Sampletype
 
 # Now prepare the interaction terms as variable to test
-
 TTCS <- interaction(Treatment,  as.factor(Timepoint) , as.factor(Concentration) , Sampletype )
 TTC <- interaction(Treatment,  as.factor(Timepoint) , as.factor(Concentration) )
 TTS <- interaction(Treatment,  as.factor(Timepoint) , Sampletype )
@@ -180,7 +147,6 @@ CMRE5_r8100_t_ja_adonis_CS <- pairwise.adonis(CMRE5_r8100_t_ja,  CS)
 
 ###########################--###########################--###########################--###########################--
 
-
 #################################-----CAP analysis on each compartment separately----------#####################
 
 ############## Mouse 
@@ -192,7 +158,6 @@ CMRE5_r8100_mouse1_t  = transform_sample_counts(CMRE5_r8100_mouse1, function(x) 
 CMRE5_r8100_mouse1_t_br <- distance(CMRE5_r8100_mouse1_t, method = "bray") ##Calculate Bray-curtis 
 
 ###########----CAP analysis
-
 CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_mouse1_t, 
                             method = "CAP",
                             distance = CMRE5_r8100_mouse1_t_br,
@@ -200,7 +165,6 @@ CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_mouse1_t,
 anova(CMRE5_r8100_cap)
 
 ###########----CAP plot
-
 CMRE5_r8100_plot_bray_cap_mouse = plot_ordination(CMRE5_r8100_mouse1_t, CMRE5_r8100_cap, color="Treatment")
 orddata <- CMRE5_r8100_plot_bray_cap_mouse$data
 centroids <- aggregate(cbind(CAP1,CAP2)~Treatment,data=orddata,mean)
@@ -210,20 +174,17 @@ gg$Treatment<- factor(gg$Treatment, levels = c("Ctr", "As", "Bx", "Tb"))
 CMRE5_r8100_plot_bray_cap_mouse = ggplot(gg, aes(x=CAP1, y=CAP2, color=Treatment)) + theme_set(theme_bw()) + theme(axis.title.x = element_text(size=15), axis.title.y = element_text(size=15), axis.text.x = element_text(size=15), axis.text.y = element_text(size=15)) + geom_point(size=2.00) + geom_point(data=centroid, mapping= aes(x=CAP1, y=CAP2, size=0.50)) + theme(legend.text=element_text(size=12), legend.title=element_text(size=14))  + scale_color_manual(values=c("gray50", "#E69F00", "#009E73", "#D55E00")) + theme(plot.title = element_text(size=20, hjust = 0.5))  + scale_shape_discrete(solid=T)  + theme(plot.title = element_text(size=15, hjust = 0.5))  + geom_segment(aes(x=CAP1.centroid, y=CAP2.centroid, xend=CAP1, yend=CAP2, color=Treatment), alpha=0.4) + theme(legend.position = "none")
 
 ###########----dispersion using betadisper (PERMDISP)# Bray-Curtis
-
 CMRE5_r8100_t_disp <- as(sample_data(CMRE5_r8100_mouse1_t), "data.frame")
 groups <- CMRE5_r8100_t_disp[["Treatment"]]
 mod1 <- betadisper(CMRE5_r8100_mouse1_t_br, groups)
 
 ############## Water 
-
 CMRE5_r8100_Water <- subset_samples(CMRE5_r8100, Sampletype%in%c("water"))
 CMRE5_r8100_Water1 = prune_taxa(taxa_sums(CMRE5_r8100_Water) > 0, CMRE5_r8100_Water) 
 
 CMRE5_r8100_Water1_t  = transform_sample_counts(CMRE5_r8100_Water1, function(x) x / sum(x) ) # transforming sample count CMRE5_r8100_Water1_t_br <- distance(CMRE5_r8100_Water1_t, method = "bray") ##Calculate Bray-curtis 
 
 ###########----CAP analysis
-
 CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Water1_t, 
                             method = "CAP",
                             distance = CMRE5_r8100_Water1_t_br,
@@ -231,7 +192,6 @@ CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Water1_t,
 anova(CMRE5_r8100_cap)
 
 ###########----CAP plot
-
 CMRE5_r8100_plot_bray_cap_Water = plot_ordination(CMRE5_r8100_Water1_t, CMRE5_r8100_cap, color="Treatment")
 orddata <- CMRE5_r8100_plot_bray_cap_Water$data
 centroids <- aggregate(cbind(CAP1,CAP2)~Treatment,data=orddata,mean)
@@ -241,13 +201,11 @@ gg$Treatment<- factor(gg$Treatment, levels = c("Ctr", "As", "Bx", "Tb"))
 CMRE5_r8100_plot_bray_cap_Water = ggplot(gg, aes(x=CAP1, y=CAP2, color=Treatment)) + theme_set(theme_bw()) + theme(axis.title.x = element_text(size=15), axis.title.y = element_text(size=15), axis.text.x = element_text(size=15), axis.text.y = element_text(size=15)) + geom_point(size=2.00) + geom_point(data=centroid, mapping= aes(x=CAP1, y=CAP2, size=0.50)) + theme(legend.text=element_text(size=12), legend.title=element_text(size=14))  + scale_color_manual(values=c("gray50", "#E69F00", "#009E73", "#D55E00")) + theme(plot.title = element_text(size=20, hjust = 0.5))  + scale_shape_discrete(solid=T)  + theme(plot.title = element_text(size=15, hjust = 0.5))  + geom_segment(aes(x=CAP1.centroid, y=CAP2.centroid, xend=CAP1, yend=CAP2, color=Treatment), alpha=0.4) + theme(legend.position = "none")
 
 ###########----dispersion using betadisper (PERMDISP)# Bray-Curtis
-
 CMRE5_r8100_t_disp <- as(sample_data(CMRE5_r8100_Water1_t), "data.frame")
 groups <- CMRE5_r8100_t_disp[["Treatment"]]
 mod1 <- betadisper(CMRE5_r8100_Water1_t_br, groups)
 
 ############## Sediment 
-
 CMRE5_r8100_Sediment <- subset_samples(CMRE5_r8100, Sampletype%in%c("feces"))
 CMRE5_r8100_Sediment1 = prune_taxa(taxa_sums(CMRE5_r8100_Sediment) > 0, CMRE5_r8100_Sediment) 
 
@@ -255,7 +213,6 @@ CMRE5_r8100_Sediment1_t  = transform_sample_counts(CMRE5_r8100_Sediment1, functi
 CMRE5_r8100_Sediment1_t_br <- distance(CMRE5_r8100_Sediment1_t, method = "bray") ##Calculate Bray-curtis 
 
 ###########----CAP analysis
-
 CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Sediment1_t, 
                             method = "CAP",
                             distance = CMRE5_r8100_Sediment1_t_br,
@@ -263,7 +220,6 @@ CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Sediment1_t,
 anova(CMRE5_r8100_cap)
 
 ###########----CAP plot
-
 CMRE5_r8100_plot_bray_cap_Sediment = plot_ordination(CMRE5_r8100_Sediment1_t, CMRE5_r8100_cap, color="Treatment")
 orddata <- CMRE5_r8100_plot_bray_cap_Sediment$data
 centroids <- aggregate(cbind(CAP1,CAP2)~Treatment,data=orddata,mean)
@@ -273,13 +229,11 @@ gg$Treatment<- factor(gg$Treatment, levels = c("Ctr", "As", "Bx", "Tb"))
 CMRE5_r8100_plot_bray_cap_Sediment = ggplot(gg, aes(x=CAP1, y=CAP2, color=Treatment)) + theme_set(theme_bw()) + theme(axis.title.x = element_text(size=15), axis.title.y = element_text(size=15), axis.text.x = element_text(size=15), axis.text.y = element_text(size=15)) + geom_point(size=2.00) + geom_point(data=centroid, mapping= aes(x=CAP1, y=CAP2, size=0.50)) + theme(legend.text=element_text(size=12), legend.title=element_text(size=14))  + scale_color_manual(values=c("gray50", "#E69F00", "#009E73", "#D55E00")) + theme(plot.title = element_text(size=20, hjust = 0.5))  + scale_shape_discrete(solid=T)  + theme(plot.title = element_text(size=15, hjust = 0.5))  + geom_segment(aes(x=CAP1.centroid, y=CAP2.centroid, xend=CAP1, yend=CAP2, color=Treatment), alpha=0.4) + theme(legend.position = "none")
 
 ###########----dispersion using betadisper (PERMDISP)# Bray-Curtis
-
 CMRE5_r8100_t_disp <- as(sample_data(CMRE5_r8100_Sediment1_t), "data.frame")
 groups <- CMRE5_r8100_t_disp[["Treatment"]]
 mod1 <- betadisper(CMRE5_r8100_Sediment1_t_br, groups)
 
 ############## Soil 
-
 CMRE5_r8100_Soil <- subset_samples(CMRE5_r8100, Sampletype%in%c("feces"))
 CMRE5_r8100_Soil1 = prune_taxa(taxa_sums(CMRE5_r8100_Soil) > 0, CMRE5_r8100_Soil) 
 
@@ -287,7 +241,6 @@ CMRE5_r8100_Soil1_t  = transform_sample_counts(CMRE5_r8100_Soil1, function(x) x 
 CMRE5_r8100_Soil1_t_br <- distance(CMRE5_r8100_Soil1_t, method = "bray") ##Calculate Bray-curtis 
 
 ###########----CAP analysis
-
 CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Soil1_t, 
                             method = "CAP",
                             distance = CMRE5_r8100_Soil1_t_br,
@@ -295,7 +248,6 @@ CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Soil1_t,
 anova(CMRE5_r8100_cap)
 
 ###########----CAP plot
-
 CMRE5_r8100_plot_bray_cap_Soil = plot_ordination(CMRE5_r8100_Soil1_t, CMRE5_r8100_cap, color="Treatment")
 orddata <- CMRE5_r8100_plot_bray_cap_Soil$data
 centroids <- aggregate(cbind(CAP1,CAP2)~Treatment,data=orddata,mean)
@@ -305,13 +257,11 @@ gg$Treatment<- factor(gg$Treatment, levels = c("Ctr", "As", "Bx", "Tb"))
 CMRE5_r8100_plot_bray_cap_Soil = ggplot(gg, aes(x=CAP1, y=CAP2, color=Treatment)) + theme_set(theme_bw()) + theme(axis.title.x = element_text(size=15), axis.title.y = element_text(size=15), axis.text.x = element_text(size=15), axis.text.y = element_text(size=15)) + geom_point(size=2.00) + geom_point(data=centroid, mapping= aes(x=CAP1, y=CAP2, size=0.50)) + theme(legend.text=element_text(size=12), legend.title=element_text(size=14))  + scale_color_manual(values=c("gray50", "#E69F00", "#009E73", "#D55E00")) + theme(plot.title = element_text(size=20, hjust = 0.5))  + scale_shape_discrete(solid=T)  + theme(plot.title = element_text(size=15, hjust = 0.5))  + geom_segment(aes(x=CAP1.centroid, y=CAP2.centroid, xend=CAP1, yend=CAP2, color=Treatment), alpha=0.4) + theme(legend.position = "none")
 
 ###########----dispersion using betadisper (PERMDISP)# Bray-Curtis
-
 CMRE5_r8100_t_disp <- as(sample_data(CMRE5_r8100_Soil1_t), "data.frame")
 groups <- CMRE5_r8100_t_disp[["Treatment"]]
 mod1 <- betadisper(CMRE5_r8100_Soil1_t_br, groups)
 
 ############## Root 
-
 CMRE5_r8100_Root <- subset_samples(CMRE5_r8100, Sampletype%in%c("feces"))
 CMRE5_r8100_Root1 = prune_taxa(taxa_sums(CMRE5_r8100_Root) > 0, CMRE5_r8100_Root) 
 
@@ -319,7 +269,6 @@ CMRE5_r8100_Root1_t  = transform_sample_counts(CMRE5_r8100_Root1, function(x) x 
 CMRE5_r8100_Root1_t_br <- distance(CMRE5_r8100_Root1_t, method = "bray") ##Calculate the Bray-curtis 
 
 ###########----CAP analysis
-
 CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Root1_t, 
                             method = "CAP",
                             distance = CMRE5_r8100_Root1_t_br,
@@ -327,7 +276,6 @@ CMRE5_r8100_cap <- ordinate(physeq = CMRE5_r8100_Root1_t,
 anova(CMRE5_r8100_cap)
 
 ###########----CAP plot
-
 CMRE5_r8100_plot_bray_cap_Root = plot_ordination(CMRE5_r8100_Root1_t, CMRE5_r8100_cap, color="Treatment")
 orddata <- CMRE5_r8100_plot_bray_cap_Root$data
 centroids <- aggregate(cbind(CAP1,CAP2)~Treatment,data=orddata,mean)
@@ -337,14 +285,12 @@ gg$Treatment<- factor(gg$Treatment, levels = c("Ctr", "As", "Bx", "Tb"))
 CMRE5_r8100_plot_bray_cap_Root = ggplot(gg, aes(x=CAP1, y=CAP2, color=Treatment)) + theme_set(theme_bw()) + theme(axis.title.x = element_text(size=15), axis.title.y = element_text(size=15), axis.text.x = element_text(size=15), axis.text.y = element_text(size=15)) + geom_point(size=2.00) + geom_point(data=centroid, mapping= aes(x=CAP1, y=CAP2, size=0.50)) + theme(legend.text=element_text(size=12), legend.title=element_text(size=14))  + scale_color_manual(values=c("gray50", "#E69F00", "#009E73", "#D55E00")) + theme(plot.title = element_text(size=20, hjust = 0.5))  + scale_shape_discrete(solid=T)  + theme(plot.title = element_text(size=15, hjust = 0.5))  + geom_segment(aes(x=CAP1.centroid, y=CAP2.centroid, xend=CAP1, yend=CAP2, color=Treatment), alpha=0.4) + theme(legend.position = "none")
 
 ###########----dispersion using betadisper (PERMDISP)# Bray-Curtis
-
 CMRE5_r8100_t_disp <- as(sample_data(CMRE5_r8100_Root1_t), "data.frame")
 groups <- CMRE5_r8100_t_disp[["Treatment"]]
 mod1 <- betadisper(CMRE5_r8100_Root1_t_br, groups)
 
 
 ###############---Now make combined figure for all beta cap compartments-------##############
-
 A <- ggarrange(CMRE5_r8100_plot_bray_cap_Water, CMRE5_r8100_plot_bray_cap_Sediment, ncol = 3) 
 B <- ggarrange(CMRE5_r8100_plot_bray_cap_Soil, CMRE5_r8100_plot_bray_cap_Root, CMRE5_r8100_plot_bray_cap_mouse, ncol = 3)
 ggarrange (A, B, ncol = 1)
