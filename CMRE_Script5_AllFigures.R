12259cc644841a532b7bc81d44062b1eb22c7eff
wasimbt <wasim.bt@gmail.com>
2024-03-21 11:51:39 +0100

    Update CMRE_Script5_AllFigures.R

--- CMRE_Script5_AllFigures.R
+++ CMRE_Script5_AllFigures.R
@@ -94,7 +94,7 @@ sample_data(CMRE5_r8100)$Treatment <- factor(sample_data(CMRE5_r8100)$Treatment,
                                              levels = c("Ctr", "As", "Bx", "Tb" )) # ordering samples
 ```
 
-# heatmap filtered data (1k abundant ASVs)  
+# heatmap filtered data (1k abu  ndant ASVs)  
 
 ## data normalization
 # 1.) express as %RA.   
@@ -2198,7 +2198,7 @@ plot_FigS5B <- plot_FigS5B_prep +
   geom_segment(aes(x=13,xend=15+0.8,
                    y=MeanShannon[13],
                    yend=MeanShannon[13])) +
-  theme_bw() + theme(panel.grid.minor.y = element_line(linewidth = 0.25, linetype = 1)) + theme(axis.title.x = element_text(size=18), axis.title.y = element_text(size=14, face = "bold"), axis.text.x = element_text(size=14, face = "bold"), axis.text.y = element_text(size=12, face = "bold")) + theme(plot.margin = unit(c(1,1,-1.5,0), "lines"))
+  theme_bw() + theme(panel.grid.minor.y = element_line(linewidth = 0.25, linetype = 1), legend.title = element_text(size=18), legend.text = element_text(size=16)) + theme(axis.title.x = element_text(size=18), axis.title.y = element_text(size=18), axis.text.x = element_text(size=14, face = "bold"), axis.text.y = element_text(size=12, face = "bold")) + theme(plot.margin = unit(c(1,1,-1.5,0), "lines"))
 
 
 plot_FigS5B
@@ -2208,7 +2208,7 @@ require(patchwork)
 plot_FigS5A + plot_FigS5B
 
 ggplot2::ggsave("Fig.S5_Suppl_Shannon.tiff", 
-                width = 10.80, height = 6.30, dpi=300)
+                width = 11.80, height = 6.30, dpi=300)
 
 
 ##############################################################################################################################
@@ -2466,6 +2466,217 @@ CMRE5_r8100_pairlist_bray_boxplot <- ggplot(CMRE5_r8100_pairlist, aes(Sampletype
 CMRE5_r8100_pairlist_bray_boxplot1 <- CMRE5_r8100_pairlist_bray_boxplot + scale_x_discrete(labels=c("Ctr As Bx Tb \nWater", "Ctr As Bx Tb \nSediment", "Ctr As Bx Tb \nSoil", "Ctr As Bx Tb  \nPlant", "Ctr As Bx Tb \nAnimal"))
 CMRE5_r8100_pairlist_bray_boxplot2 <- CMRE5_r8100_pairlist_bray_boxplot1 + stat_summary(fun.y=mean, geom="point", colour="black", shape=18, size=2, position=position_dodge(width=0.75))  
 
+##############################################################################################
+
+##############################################################################################################################
+
+##############################################################################################################################
+
+##########################################------Supplementary Figure S7---------###############################################
+
+#### water 
+
+#water_CtrAs
+ 
+Water_CtrAs = read.csv(file = "Water_CtrAs_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Water_CtrAs1 <- Water_CtrAs %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Water_CtrAs1$Treatment <- rep("As",nrow(Water_CtrAs1)) #make new column Treatment and fill all As
+write.csv(Water_CtrAs1, file = "Water_CtrAs_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+#Water_CtrBx ---no taxa present
+#Water_CtrTb
+ 
+Water_CtrTb = read.csv(file = "Water_CtrTb_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created 
+Water_CtrTb1 <- Water_CtrTb %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Water_CtrTb1$Treatment <- rep("Tb",nrow(Water_CtrTb1)) #make new column Treatment and fill all Tb
+write.csv(Water_CtrTb1, file = "Water_CtrTb_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+ #merge all  
+ Water_CtrAsBxTb_deseq2_sigOTU05_3 <- rbind(Water_CtrAs1, Water_CtrTb1)
+
+#### prepare plot#####
+  
+ Water_CtrAsBxTb = read.csv(file = "Water_CtrAsBxTb_deseq2_sigOTU05_3.csv", header = TRUE, sep = ',')
+ Water_CtrAsBxTb1 <- transform(Water_CtrAsBxTb, ASV_ID1=paste(Treatment, ASV_ID, sep="_"))
+ Water_CtrAsBxTb1$Treatment <- factor(Water_CtrAsBxTb1$Treatment, levels = c("As", "Bx", "Tb"))
+ x = tapply(Water_CtrAsBxTb1$log2FoldChange, Water_CtrAsBxTb1$ASV_ID1, function(x) max(x))
+ x = sort(x, TRUE)
+ Water_CtrAsBxTb1$ASV_ID1 = factor(as.character(Water_CtrAsBxTb1$ASV_ID1), levels = names(x))
+ ##Plot 1
+ p1 <-   ggplot(Water_CtrAsBxTb1, aes(x = log2FoldChange, y = ASV_ID1)) + 
+   geom_vline(xintercept = 0.0, color = "gray", linewidth = 1.5) + geom_point(data=Water_CtrAsBxTb1, aes(colour = log2FoldChange >0), size = 3, alpha=0.7) + theme(axis.title.y=element_blank()) + theme(axis.text.x = element_text(hjust = 0, vjust = 0.5), plot.title = element_text(hjust = 0.5)) + labs(x= "log2FoldChange", y = "ASV-ID") + theme_bw() + theme(axis.title.x = element_text(size=16), axis.title.y = element_text(size=16), axis.text.x  = element_text(colour="black", face = "bold", vjust=0.5, size=14), axis.text.y  = element_text(colour="black", face = "bold", vjust=0.5, size=8)) + labs(color='log2FC') + scale_x_continuous(limits=c(-25, 5), breaks=c(-25, -20, -15, -10, -5, 0, 5)) + theme(legend.position="none")
+ ##Plot 2
+ p2 <-p1 + scale_colour_manual(name = 'log2FoldChange > 0', values = setNames(c("#1465AC",  "#B31B21"),c(T, F))) + theme(axis.text.x = element_text( hjust = 0, vjust=0.5)) 
+ ##Plot 3
+ p3 <- p2 + scale_y_discrete(labels = function(labels) sapply(labels, function(label) {
+   parts <- strsplit(as.character(label), "_")[[1]]
+   if (length(parts) > 1) {
+     paste(parts[-1], collapse = "_")  # Re-join parts if there's more than one underscore
+   } else {
+     label  # Use the original label if there's no underscore
+   }
+ }))
+ ##Final plot
+ p4 <-p3+ facet_grid(Treatment~.,scales = "free", space = "free", switch = "y")+ theme() +
+   theme(strip.placement = "outside")  + theme(strip.text.y = element_text(angle = 0)) + theme(strip.text.y = element_text(angle = 180, size=11, colour="black", face = "bold")) + theme(strip.background = element_rect(fill="lightblue", colour="black"), plot.title = element_text(size=16, face="bold",hjust=0.25, vjust = 0.10)) + ggtitle("Water")
+
+#### Sediment 
+
+#Sediment_CtrAs
+ 
+Sediment_CtrAs = read.csv(file = "Sediment_CtrAs_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Sediment_CtrAs1 <- Sediment_CtrAs %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Sediment_CtrAs1$Treatment <- rep("As",nrow(Sediment_CtrAs1)) #make new column Treatment and fill all As
+write.csv(Sediment_CtrAs1, file = "Sediment_CtrAs_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+#Sediment_CtrBx 
+
+Sediment_CtrBx = read.csv(file = "Sediment_CtrBx_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Sediment_CtrBx1 <- Sediment_CtrBx %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Sediment_CtrBx1$Treatment <- rep("Bx",nrow(Sediment_CtrBx1)) #make new column Treatment and fill all As
+write.csv(Sediment_CtrBx1, file = "Sediment_CtrBx_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+                             
+#Sediment_CtrTb
+ 
+Sediment_CtrTb = read.csv(file = "Sediment_CtrTb_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created 
+Sediment_CtrTb1 <- Sediment_CtrTb %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Sediment_CtrTb1$Treatment <- rep("Tb",nrow(Sediment_CtrTb1)) #make new column Treatment and fill all Tb
+write.csv(Sediment_CtrTb1, file = "Sediment_CtrTb_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+ #merge all  
+ Sediment_CtrAsBxTb_deseq2_sigOTU05_3 <- rbind(Sediment_CtrAs1, Sediment_CtrBx1, Sediment_CtrTb1)
+
+#### prepare plot#####
+  
+ Sediment_CtrAsBxTb = read.csv(file = "Sediment_CtrAsBxTb_deseq2_sigOTU05_3.csv", header = TRUE, sep = ',')
+ Sediment_CtrAsBxTb1 <- transform(Sediment_CtrAsBxTb, ASV_ID1=paste(Treatment, ASV_ID, sep="_"))
+ Sediment_CtrAsBxTb1$Treatment <- factor(Sediment_CtrAsBxTb1$Treatment, levels = c("As", "Bx", "Tb"))
+ x = tapply(Sediment_CtrAsBxTb1$log2FoldChange, Sediment_CtrAsBxTb1$ASV_ID1, function(x) max(x))
+ x = sort(x, TRUE)
+ Sediment_CtrAsBxTb1$ASV_ID1 = factor(as.character(Sediment_CtrAsBxTb1$ASV_ID1), levels = names(x))
+ ##Plot 1
+ p1 <-   ggplot(Sediment_CtrAsBxTb1, aes(x = log2FoldChange, y = ASV_ID1)) + 
+   geom_vline(xintercept = 0.0, color = "gray", linewidth = 1.5) + geom_point(data=Sediment_CtrAsBxTb1, aes(colour = log2FoldChange >0), size = 3, alpha=0.7) + theme(axis.title.y=element_blank()) + theme(axis.text.x = element_text(hjust = 0, vjust = 0.5), plot.title = element_text(hjust = 0.5)) + labs(x= "log2FoldChange", y = "ASV-ID") + theme_bw() + theme(axis.title.x = element_text(size=16), axis.title.y = element_text(size=16), axis.text.x  = element_text(colour="black", face = "bold", vjust=0.5, size=14), axis.text.y  = element_text(colour="black", face = "bold", vjust=0.5, size=8)) + labs(color='log2FC') + scale_x_continuous(limits=c(-20, 6), breaks=c(-20, -15, -10, -5,0, 5)) + theme(legend.position="none")
+ ##Plot 2
+ p2 <-p1 + scale_colour_manual(name = 'log2FoldChange > 0', values = setNames(c("#1465AC",  "#B31B21"),c(T, F))) + theme(axis.text.x = element_text( hjust = 0, vjust=0.5)) 
+ ##Plot 3
+ p3 <- p2 + scale_y_discrete(labels = function(labels) sapply(labels, function(label) {
+   parts <- strsplit(as.character(label), "_")[[1]]
+   if (length(parts) > 1) {
+     paste(parts[-1], collapse = "_")  # Re-join parts if there's more than one underscore
+   } else {
+     label  # Use the original label if there's no underscore
+   }
+ }))
+ ##Final plot
+ p4 <-p3+ facet_grid(Treatment~.,scales = "free", space = "free", switch = "y")+ theme() +
+   theme(strip.placement = "outside")  + theme(strip.text.y = element_text(angle = 0)) + theme(strip.text.y = element_text(angle = 180, size=11, colour="black", face = "bold")) + theme(strip.background = element_rect(fill="lightblue", colour="black"), plot.title = element_text(size=16, face="bold",hjust=0.25, vjust = 0.10)) + ggtitle("Sediment")
+
+
+#### Soil 
+
+#Soil_CtrAs
+ 
+Soil_CtrAs = read.csv(file = "Soil_CtrAs_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Soil_CtrAs1 <- Soil_CtrAs %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Soil_CtrAs1$Treatment <- rep("As",nrow(Soil_CtrAs1)) #make new column Treatment and fill all As
+write.csv(Soil_CtrAs1, file = "Soil_CtrAs_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+#Soil_CtrBx 
+
+Soil_CtrBx = read.csv(file = "Soil_CtrBx_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Soil_CtrBx1 <- Soil_CtrBx %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Soil_CtrBx1$Treatment <- rep("Bx",nrow(Soil_CtrBx1)) #make new column Treatment and fill all As
+write.csv(Soil_CtrBx1, file = "Soil_CtrBx_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+                             
+#Soil_CtrTb
+ 
+Soil_CtrTb = read.csv(file = "Soil_CtrTb_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created 
+Soil_CtrTb1 <- Soil_CtrTb %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Soil_CtrTb1$Treatment <- rep("Tb",nrow(Soil_CtrTb1)) #make new column Treatment and fill all Tb
+write.csv(Soil_CtrTb1, file = "Soil_CtrTb_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+ #merge all  
+ Soil_CtrAsBxTb_deseq2_sigOTU05_3 <- rbind(Soil_CtrAs1, Soil_CtrBx1, Soil_CtrTb1)
+
+#### prepare plot#####
+  
+ Soil_CtrAsBxTb = read.csv(file = "Soil_CtrAsBxTb_deseq2_sigOTU05_3.csv", header = TRUE, sep = ',')
+ Soil_CtrAsBxTb1 <- transform(Soil_CtrAsBxTb, ASV_ID1=paste(Treatment, ASV_ID, sep="_"))
+ Soil_CtrAsBxTb1$Treatment <- factor(Soil_CtrAsBxTb1$Treatment, levels = c("As", "Bx", "Tb"))
+ x = tapply(Soil_CtrAsBxTb1$log2FoldChange, Soil_CtrAsBxTb1$ASV_ID1, function(x) max(x))
+ x = sort(x, TRUE)
+ Soil_CtrAsBxTb1$ASV_ID1 = factor(as.character(Soil_CtrAsBxTb1$ASV_ID1), levels = names(x))
+ ##Plot 1
+ p1 <-   ggplot(Soil_CtrAsBxTb1, aes(x = log2FoldChange, y = ASV_ID1)) + 
+   geom_vline(xintercept = 0.0, color = "gray", linewidth = 1.5) + geom_point(data=Soil_CtrAsBxTb1, aes(colour = log2FoldChange >0), size = 3, alpha=0.7) + theme(axis.title.y=element_blank()) + theme(axis.text.x = element_text(hjust = 0, vjust = 0.5), plot.title = element_text(hjust = 0.5)) + labs(x= "log2FoldChange", y = "ASV-ID") + theme_bw() + theme(axis.title.x = element_text(size=16), axis.title.y = element_text(size=16), axis.text.x  = element_text(colour="black", face = "bold", vjust=0.5, size=14), axis.text.y  = element_text(colour="black", face = "bold", vjust=0.5, size=8)) + labs(color='log2FC') + scale_x_continuous(limits=c(-2, 4), breaks=c(-2, 1, 0, 1, 2, 3, 4)) + theme(legend.position="none")
+ ##Plot 2
+ p2 <-p1 + scale_colour_manual(name = 'log2FoldChange > 0', values = setNames(c("#1465AC",  "#B31B21"),c(T, F))) + theme(axis.text.x = element_text( hjust = 0, vjust=0.5)) 
+ ##Plot 3
+ p3 <- p2 + scale_y_discrete(labels = function(labels) sapply(labels, function(label) {
+   parts <- strsplit(as.character(label), "_")[[1]]
+   if (length(parts) > 1) {
+     paste(parts[-1], collapse = "_")  # Re-join parts if there's more than one underscore
+   } else {
+     label  # Use the original label if there's no underscore
+   }
+ }))
+ ##Final plot
+ p4 <-p3+ facet_grid(Treatment~.,scales = "free", space = "free", switch = "y")+ theme() +
+   theme(strip.placement = "outside")  + theme(strip.text.y = element_text(angle = 0)) + theme(strip.text.y = element_text(angle = 180, size=11, colour="black", face = "bold")) + theme(strip.background = element_rect(fill="lightblue", colour="black"), plot.title = element_text(size=16, face="bold",hjust=0.25, vjust = 0.10)) + ggtitle("Soil")
+
+#### Animal 
+
+#Mouse_CtrAs
+ 
+Mouse_CtrAs = read.csv(file = "Mouse_CtrAs_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Mouse_CtrAs1 <- Mouse_CtrAs %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Mouse_CtrAs1$Treatment <- rep("As",nrow(Mouse_CtrAs1)) #make new column Treatment and fill all As
+write.csv(Mouse_CtrAs1, file = "Mouse_CtrAs_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+#Mouse_CtrBx 
+
+Mouse_CtrBx = read.csv(file = "Mouse_CtrBx_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created. Made new column of Tax and ASV-ID
+Mouse_CtrBx1 <- Mouse_CtrBx %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Mouse_CtrBx1$Treatment <- rep("Bx",nrow(Mouse_CtrBx1)) #make new column Treatment and fill all As
+write.csv(Mouse_CtrBx1, file = "Mouse_CtrBx_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+                             
+#Mouse_CtrTb
+ 
+Mouse_CtrTb = read.csv(file = "Mouse_CtrTb_deseq2_sigOTU05_2.csv", header = TRUE, sep = ',') # these files are manually created 
+Mouse_CtrTb1 <- Mouse_CtrTb %>% mutate(ASV_ID = paste(Tax, ASV.ID, sep = "_"))
+Mouse_CtrTb1$Treatment <- rep("Tb",nrow(Mouse_CtrTb1)) #make new column Treatment and fill all Tb
+write.csv(Mouse_CtrTb1, file = "Mouse_CtrTb_deseq2_sigOTU05_3.CSV", row.names = TRUE, sep = ',', col.names = TRUE) #save As CSV file
+ 
+ #merge all  
+ Mouse_CtrAsBxTb_deseq2_sigOTU05_3 <- rbind(Mouse_CtrAs1, Mouse_CtrBx1, Mouse_CtrTb1)
+
+#### prepare plot#####
+  
+ Mouse_CtrAsBxTb = read.csv(file = "Mouse_CtrAsBxTb_deseq2_sigOTU05_3.csv", header = TRUE, sep = ',')
+ Mouse_CtrAsBxTb1 <- transform(Mouse_CtrAsBxTb, ASV_ID1=paste(Treatment, ASV_ID, sep="_"))
+ Mouse_CtrAsBxTb1$Treatment <- factor(Mouse_CtrAsBxTb1$Treatment, levels = c("As", "Bx", "Tb"))
+ x = tapply(Mouse_CtrAsBxTb1$log2FoldChange, Mouse_CtrAsBxTb1$ASV_ID1, function(x) max(x))
+ x = sort(x, TRUE)
+ Mouse_CtrAsBxTb1$ASV_ID1 = factor(as.character(Mouse_CtrAsBxTb1$ASV_ID1), levels = names(x))
+ ##Plot 1
+ p1 <-   ggplot(Mouse_CtrAsBxTb1, aes(x = log2FoldChange, y = ASV_ID1)) + 
+   geom_vline(xintercept = 0.0, color = "gray", linewidth = 1.5) + geom_point(data=Mouse_CtrAsBxTb1, aes(colour = log2FoldChange >0), size = 3, alpha=0.7) + theme(axis.title.y=element_blank()) + theme(axis.text.x = element_text(hjust = 0, vjust = 0.5), plot.title = element_text(hjust = 0.5)) + labs(x= "log2FoldChange", y = "ASV-ID") + theme_bw() + theme(axis.title.x = element_text(size=16), axis.title.y = element_text(size=16), axis.text.x  = element_text(colour="black", face = "bold", vjust=0.5, size=14), axis.text.y  = element_text(colour="black", face = "bold", vjust=0.5, size=8)) + labs(color='log2FC') + scale_x_continuous(limits=c(-2, 4), breaks=c(-2, 1, 0, 1, 2, 3, 4)) + theme(legend.position="none")
+ ##Plot 2
+ p2 <-p1 + scale_colour_manual(name = 'log2FoldChange > 0', values = setNames(c("#1465AC",  "#B31B21"),c(T, F))) + theme(axis.text.x = element_text( hjust = 0, vjust=0.5)) 
+ ##Plot 3
+ p3 <- p2 + scale_y_discrete(labels = function(labels) sapply(labels, function(label) {
+   parts <- strsplit(as.character(label), "_")[[1]]
+   if (length(parts) > 1) {
+     paste(parts[-1], collapse = "_")  # Re-join parts if there's more than one underscore
+   } else {
+     label  # Use the original label if there's no underscore
+   }
+ }))
+ ##Final plot
+ p4 <-p3+ facet_grid(Treatment~.,scales = "free", space = "free", switch = "y")+ theme() +
+   theme(strip.placement = "outside")  + theme(strip.text.y = element_text(angle = 0)) + theme(strip.text.y = element_text(angle = 180, size=11, colour="black", face = "bold")) + theme(strip.background = element_rect(fill="lightblue", colour="black"), plot.title = element_text(size=16, face="bold",hjust=0.25, vjust = 0.10)) + ggtitle("Animal")
+                              
 ##############################################################################################
 
 ##############################################################################################################################
